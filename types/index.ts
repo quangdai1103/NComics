74effb6fa502e0df7ef933ff159f6f9745539f8e
quangdai1103 <104144863+quangdai1103@users.noreply.github.com>
2023-10-20 23:11:33 +0700

    Add files via upload

--- types/index.ts
+++ types/index.ts
@@ -10,6 +10,7 @@ export type ComicDetail = {
     id: string;
     name: string;
   }[];
+  is_adult: boolean;
   other_names: string[];
   rating_count: number;
   id: string;
@@ -23,22 +24,52 @@ export type ComicDetail = {
 export type Comic = {
   title: string;
   thumbnail: string;
-  followers: number;
+  followers: string;
   genres: {
     id: string;
     name: string;
   }[];
+  authors: string | string[];
   id: string;
   is_trending: boolean;
   last_chapter: {
     id: string;
     name: string;
+    updated_at: string;
   };
   other_names: string[];
   short_description: string;
   status: string;
-  total_views: number;
+  total_comments: string;
+  total_views: string;
   updated_at: string;
+};
+
+export type Reply = {
+  avatar: string;
+  content: string;
+  created_at: string;
+  username: string;
+  stickers: string[];
+  vote_count: number;
+  mention_user: string;
+};
+
+export type Comment = {
+  avatar: string;
+  content: string;
+  created_at: string;
+  username: string;
+  stickers: string[];
+  vote_count: number;
+  replies: Reply[];
+};
+
+export type ComicComments = {
+  total_pages: number;
+  total_comments: number;
+  current_page: number;
+  comments: Comment[];
 };
 
 export type Genre = {
